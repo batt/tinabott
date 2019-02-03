c736b7689d35ef661c00c3d2775d65add2346cfe
Francesco Sacchi <batt@develer.com>
2019-02-03 15:36:15 +0100

    First commit.

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/batt/tinabott/slackbot"
+	"github.com/nlopes/slack"
+)
+
+func main() {
+	token := os.Getenv("SLACK_BOT_TOKEN")
+
+	if token == "" {
+		log.Fatalln("No slack token found!")
+	}
+
+	// Slack Bot filter
+	var opts slackbot.Config
+	bot := slackbot.New(token, opts)
+
+	bot.RespondTo("^per me (.*)$", func(b *slackbot.Bot, msg *slack.Msg, user *slack.User, args ...string) {
+		fmt.Printf("Message from channel (%s) <%s>: %s", msg.Channel, user.Name, msg.Text)
+		bot.Message(msg.Channel, "Ok, "+args[1]+" per "+user.Name)
+	})
+
+	fmt.Printf("Run Bot server\n\r")
+	go func(b *slackbot.Bot) {
+		if err := b.Start(); err != nil {
+			log.Fatalln(err)
+		}
+	}(bot)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "8080"
+	}
+
+	httpURL := os.Getenv("HTTPURL")
+	if httpURL == "" {
+		httpURL = "https://tinabott.herokuapp.com"
+	}
+
+	http.HandleFunc("/keepalive", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "OK")
+		fmt.Println("keepalice pong")
+	})
+
+	wakeUpTime := os.Getenv("WAKEUP_TIME")
+	if wakeUpTime == "" {
+		wakeUpTime = "6:00"
+	}
+
+	sleepTime := os.Getenv("SLEEP_TIME")
+	if sleepTime == "" {
+		sleepTime = "21:00"
+	}
+
+	w := strings.Split(wakeUpTime, ":")
+	s := strings.Split(sleepTime, ":")
+
+	wh, _ := strconv.Atoi(w[0])
+	wm, _ := strconv.Atoi(w[1])
+	wakeUpOffset := (60*wh + wm) % (60 * 24)
+
+	sh, _ := strconv.Atoi(s[0])
+	sm, _ := strconv.Atoi(s[1])
+	awakeMinutes := (60*(sh+24) + sm - wakeUpOffset) % (60 * 24)
+
+	ticker := time.NewTicker(10 * time.Minute)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				now := time.Now()
+
+				elapsedMinutes := (60*(now.Hour()+24) + now.Minute() - wakeUpOffset) % (60 * 24)
+				fmt.Printf("Awake for %d minutes\n", elapsedMinutes)
+				if elapsedMinutes < awakeMinutes {
+					fmt.Println("keepalive ping!")
+					http.Get(httpURL + "/keepalive")
+				} else {
+					fmt.Println("skipping keepalive, going to sleep...")
+				}
+			}
+		}
+	}()
+
+	fmt.Printf("Run HTTP server on port:%v\n\r", httpPort)
+	http.ListenAndServe(":"+httpPort, nil)
+}
