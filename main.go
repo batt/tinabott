6ecf8e121504457f617dba6ab27ae9416848523e
Francesco Sacchi <batt@develer.com>
2019-02-05 00:29:32 +0100

    Add "per me" and "ordine" commands

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -16,6 +17,77 @@ import (
 	"github.com/batt/tinabott/slackbot"
 	"github.com/nlopes/slack"
 )
+
+type Order struct {
+	Timestamp time.Time
+	Dishes    map[string][]string //map dishes with users
+	Users     map[string][]string //map each user to his/her dishes
+}
+
+func NewOrder() *Order {
+	return &Order{
+		Timestamp: time.Now(),
+		Dishes:    make(map[string][]string),
+		Users:     make(map[string][]string),
+	}
+}
+
+func getOrder(brain *brain.Brain) *Order {
+	var order Order
+	err := brain.Get("order", &order)
+	if err != nil {
+		return NewOrder()
+	}
+
+	if time.Since(order.Timestamp).Hours() > 13 {
+		log.Infoln("Deleting old order")
+		return NewOrder()
+	}
+	return &order
+}
+
+func fuzzyMatch(dish, menuline string) bool {
+	dish = strings.ToLower(dish)
+
+	key := regexp.MustCompile(strings.Replace(dish, " ", ".*", -1))
+
+	return key.MatchString(strings.ToLower(menuline))
+}
+
+func findDishes(menu, dish string) []string {
+	dish = strings.TrimSpace(strings.ToLower(dish))
+	menus := strings.Split(strings.TrimSpace(menu), "\n")
+	var matches []string
+	for _, m := range menus {
+		if strings.ToLower(m) == dish {
+			return []string{m}
+		}
+
+		if fuzzyMatch(dish, m) {
+			matches = append(matches, m)
+		}
+	}
+	return matches
+}
+
+func clearUserOrder(order *Order, user string) string {
+	dishes := order.Users[user]
+	delete(order.Users, user)
+	for _, d := range dishes {
+
+		// Find and remove the user
+		for i, v := range order.Dishes[d] {
+			if v == user {
+				order.Dishes[d] = append(order.Dishes[d][:i], order.Dishes[d][i+1:]...)
+				break
+			}
+		}
+		if len(order.Dishes[d]) == 0 {
+			delete(order.Dishes, d)
+		}
+	}
+	return strings.Join(dishes, ",")
+}
 
 func main() {
 	token := os.Getenv("SLACK_BOT_TOKEN")
@@ -36,8 +108,49 @@ func main() {
 	brain := brain.New(redisURL)
 
 	bot.RespondTo("^per me (.*)$", func(b *slackbot.Bot, msg *slack.Msg, user *slack.User, args ...string) {
-		fmt.Printf("Message from channel (%s) <%s>: %s\n\r", msg.Channel, user.Name, msg.Text)
-		bot.Message(msg.Channel, "Ok, "+args[1]+" per "+user.Name)
+		dish := args[1]
+		order := getOrder(brain)
+
+		if strings.ToLower(dish) == "niente" {
+			old := clearUserOrder(order, user.Name)
+			bot.Message(msg.Channel, "Ok, cancello ordine "+old)
+			brain.Set("order", order)
+			return
+		}
+		var menu string
+		err := brain.Get("menu", &menu)
+		if err != nil {
+			bot.Message(msg.Channel, "Nessun menu impostato!")
+			return
+		}
+
+		dishes := findDishes(menu, dish)
+		if len(dishes) == 0 {
+			bot.Message(msg.Channel, "Non ho trovato nulla nel menu che corrisponda a '"+dish+"'")
+		} else if len(dishes) > 1 {
+			matches := strings.Join(dishes, "\n")
+			bot.Message(msg.Channel, "Ho trovato i seguenti piatti:\n"+matches+"\n----\nSii più preciso cribbio!")
+		} else {
+			d := dishes[0]
+			u := user.Name
+			order.Dishes[d] = append(order.Dishes[d], u)
+			order.Users[u] = append(order.Users[u], d)
+			brain.Set("order", order)
+			bot.Message(msg.Channel, "Ok, "+d+" per "+u)
+		}
+	})
+
+	bot.RespondTo("^ordine$", func(b *slackbot.Bot, msg *slack.Msg, user *slack.User, args ...string) {
+		order := getOrder(brain)
+
+		r := ""
+		for d := range order.Dishes {
+			l := fmt.Sprintf("%d %s ", len(order.Dishes[d]), d)
+			l += "[ " + strings.Join(order.Dishes[d], ",") + " ]\n"
+			r = r + l
+		}
+
+		bot.Message(msg.Channel, "Ecco l'ordine:\n"+r)
 	})
 
 	bot.RespondTo("^menu([\\s\\S]*)?", func(b *slackbot.Bot, msg *slack.Msg, user *slack.User, args ...string) {
